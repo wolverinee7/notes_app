151928db9f44392eb6bd81e488477126d453f1a0
wolverinee7 <ahmedsrour600@gmail.com>
2024-05-15 10:09:01 +0300

    fixes 2

--- lib/widgets/addnote_bottomsheet.dart
+++ lib/widgets/addnote_bottomsheet.dart
@@ -26,7 +26,8 @@ class AddNoteBottomSheet extends StatelessWidget {
           return AbsorbPointer(
             absorbing: state is AddNoteLoading ? true :false,
             child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16),
+              padding: EdgeInsets.only(left: 16,right: 16,
+              bottom: MediaQuery.of(context).viewInsets.bottom),
               child: const SingleChildScrollView(
                 child: AddNoteForm(),
               ),
